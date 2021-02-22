0d500de7d854ba26eb60894c7bd8c308aca8145f
sarah-n-wright <sarahwright474@gmail.com>
2021-02-22 14:00:22 -0800

    Added folders for idividual run parameters

---
+++ RunFiles/run_gwas_pipeline_A_exome.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+#SBATCH --job-name=exome_QC
+#SBATCH --output /cellar/users/snwright/Data/SlurmOut/exome_QC/redo_%A_%a.out
+#SBATCH --error /cellar/users/snwright/Data/SlurmOut/exome_QC/redo_%A_%a.err
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=32G
+#SBATCH --time=8:00:00
+#SBATCH --array=0-24
+
+config=Exome_QC_config.conf
+chromosomes=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 'X' 'Y')
+#chromosomes=(16)
+CHR=${chromosomes[$SLURM_ARRAY_TASK_ID]}
+
+source /cellar/users/snwright/Git/gwas_pipeline/gwas_1_common_snps.sh $config $CHR
+
+echo "############################ GWAS 1 COMPLETED ##########################"
+
+source /cellar/users/snwright/Git/gwas_pipeline/gwas_2_missingness.sh $config $CHR
+
+echo "############################ GWAS 2 COMPLETED ##########################"
+
+source /cellar/users/snwright/Git/gwas_pipeline/gwas_3_check_missing_hwe.sh $config $CHR
+
+echo "############################ GWAS 3 COMPLETED ##########################"
+
+source /cellar/users/snwright/Git/gwas_pipeline/gwas_4_prune_LD.sh $config $CHR
+
+echo "############################ GWAS 4 COMPLETED ##########################"
+
