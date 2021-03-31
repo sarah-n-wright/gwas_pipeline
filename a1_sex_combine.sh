3f5a25286448934aa8c26a93201a85c1f9df4697
sarah-n-wright <sarahwright474@gmail.com>
2021-03-31 12:19:19 -0700

    Now uses updated phenotype files

--- a1_sex_combine.sh
+++ a1_sex_combine.sh
@@ -1,9 +1,7 @@
 source /cellar/users/snwright/Git/gwas_pipeline/Configs/$1 "X"
-xBed=$bedfile
-xBim=$bimfile
-xFam=$famfile
+xPref=${outDir}${outName}.updated_phe
 source /cellar/users/snwright/Git/gwas_pipeline/Configs/$1 "Y"
-
+yPref=${outDir}${outName}.updated_phe
 echo "------------------------------>Combining sex chrs<-------------------------"
 
 ### Check if sex chromosome info is split across files.
@@ -17,13 +15,15 @@ then
 # if it is merge the bed files
 		merge_list=${outDir}${baseName}.combine_sexCHR_merge_list.txt
 		touch $merge_list
-		echo $bedfile $bimfile $famfile >> $merge_list
+		echo $yPref.bed $yPref.bim $yPref.fam >> $merge_list
 		source /cellar/users/snwright/Git/gwas_pipeline/Configs/$1 "XY"
 		if test -f $bedfile
 		then
-			echo $bedfile $bimfile $famfile >> $merge_list
+			source /cellar/users/snwright/Git/gwas_pipeline/update_phenotype.sh $1 "XY"
+			xyPref=${outDir}${outName}.updated_phe
+			echo $xyPref.bed $xyPref.bim $xyPref.fam >> $merge_list
 		fi
-		srun -l plink -bfile ${xBed%".bed"} \
+		srun -l plink -bfile $xPref \
 		--merge-list $merge_list \
 		--make-bed --out ${outDir}${baseName}.combined_sexCHR
 	fi
