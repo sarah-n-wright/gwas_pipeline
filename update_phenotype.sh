d0b391400abe3cbe9a853c202fe01bb0ba1bb77d
sarah-n-wright <sarahwright474@gmail.com>
2021-03-31 12:11:18 -0700

    Allow for empty case list

--- update_phenotype.sh
+++ update_phenotype.sh
@@ -1,7 +1,11 @@
 source /cellar/users/snwright/Git/gwas_pipeline/Configs/$1 $2
 
-# make list 
-awk '{print $1 "\t" $1 > "${outDir}${outName}.cases.phe"}' $case_list
+if [ "$case_list" = "" ]
+then
+	touch ${outDir}${outName}.cases.phe
+else
+	awk '{print $1 "\t" $1 > "${outDir}${outName}.cases.phe"}' $case_list
+fi
 
 srun -l plink --bed $bedfile --bim $bimfile --fam $famfile \
 --make-pheno ${outDir}${outName}.cases.phe '*' \
