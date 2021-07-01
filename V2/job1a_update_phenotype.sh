ff11750eb2b58b4329a1c82c4a55272c2ffd7035
sarah-n-wright <sarahwright474@gmail.com>
2021-07-01 10:00:10 -0700

    Keep updte phenotype as is

---
+++ V2/job1a_update_phenotype.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH --job-name=pheno
+#SBATCH --output /cellar/users/snwright/Data/SlurmOut/pheno_%A.out
+#SBATCH --error /cellar/users/snwright/Data/SlurmOut/pheno_%A.err
+#SBATCH --partition=nrnb-compute
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=32G
+#SBATCH --time=1:00:00
+#SBATCH --array=0-25
+script_path=/nrnb/ukb-majithia/sarah/Git/gwas_pipeline/V2/
+config=$1
+chromosomes=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 'X' 'Y' 'XY' 'MT')
+#chromosomes=(19)
+CHR=${chromosomes[$SLURM_ARRAY_TASK_ID]}
+
+source ${script_path}Configs/$1 ""
+
+### Add updated phenotype ###
+
+source ${script_path}update_phenotype.sh $config $CHR $case_list
+
+
---
+++ V2/update_phenotype.sh
@@ -0,0 +1,21 @@
+source /nrnb/ukb-majithia/sarah/Git/gwas_pipeline/Configs/$1 $2
+cases=$3
+
+if [ "$cases" == "" ]
+then
+	> ${outDir}${outName}.cases.phe
+	cases=${outDir}${outName}.cases.phe
+	remove_cases=1
+else
+	remove_cases=0
+fi
+
+srun -l plink --bed $bedfile --bim $bimfile --fam $famfile \
+	--make-pheno $cases '*' \
+	--allow-no-sex \
+	--make-bed --out ${outDir}${outName}.updated_phe
+
+if [ $remove_cases -eq 1 ]
+then
+	rm $cases
+fi
