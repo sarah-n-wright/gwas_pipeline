d5566bea631b9c7439a8b4f00623290144da2c0e
sarah-n-wright <sarahwright474@gmail.com>
2021-07-29 13:53:40 -0700

    Udpate to annotation tool

--- RV0/annotation_utility.py
+++ RV0/annotation_utility.py
@@ -1,6 +1,5 @@
 import argparse
-from pandas import DataFrame
-from pandas import read_csv
+
 
 
 
@@ -49,14 +48,14 @@ class AnnotationData:
             sub_data.to_csv(self.outfile, columns=["variantID"],
                             index=False, sep="\t")
         else:
-            sub_data.to_csv(self.outfile, columns=["variantID", "Type", "GeneName", self.AnnotatorName],
+            sub_data.to_csv(self.outfile, columns=["#Chrom", "Pos", "Ref", "Alt", "variantID", "Type", "GeneName", self.AnnotatorName],
                             index=False, sep="\t")
     
     def summarize_annotator(self):
         self.load_annotation_data()
         if self.isCategorical:
             print("Unique values:", self.anno_data[self.AnnotatorName].unique())
-            print(self.anno_data[self.AnnotatorName].value_counts())
+            print(self.anno_data[self.AnnotatorName].value_counts(dropna=False))
         elif self.isQuantitative:
             print(self.anno_data[self.AnnotatorName].describe())
 
@@ -68,20 +67,24 @@ def variant_id_spec(df, chrom="#Chrom", pos="Pos", ref="Ref", alt="Alt"):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", help="path to annotation file", type=str)
-    parser.add_argument("-g", "--getAnnotators", action='store_true')
-    parser.add_argument("-s", "--summarize", help="Summarize the values of an annotator", action="store_true")
-    parser.add_argument("-a", "--annotator",help="Annotator(s) to use, space separated list of annotators")
-    parser.add_argument("-q", "--isQuantitative", action='store_true')
-    parser.add_argument("-c", "--isCategorical", action='store_true')
-    parser.add_argument("-x", "--maximum", help="Maximum value for continuous annotation", action='store', type=float, default=float('inf'))
-    parser.add_argument("-m", "--minimum", help="Minimum value for continuous annotation", action='store', type=float, default=float('inf'))
-    parser.add_argument("-i", "--include", help="space separated list of categorical values", nargs="+")
+    parser.add_argument("FILE", help="Path to annotation file", type=str)
+    parser.add_argument("-g", "--getAnnotators", help="Return the column names from FILE", action='store_true')
+    parser.add_argument("-s", "--summarize", help="Summarize the values of an annotator. Requires input ANNOTATOR to -a and one of -c/-q to specify categorical or quantitative annotations. For categorical annotations the unique values and associated counts are returned.", action="store_true")
+    parser.add_argument("-f", "--filter", help="Filter variants based on the values of an annotator. Requires input ANNOTATOR to -a, one of -c/-q to specify categorical or quantitative annotations. For categorical annotations a set of values to include must be specified via -i. For quantitative traits maximum and/or minimum values must be set via -x/-m.", action="store_true")
+    parser.add_argument("-a", "--annotator",help="Column name of Annotator of interest")
+    parser.add_argument("-q", "--isQuantitative", help="ANNOTATOR is quantitative", action='store_true')
+    parser.add_argument("-c", "--isCategorical", help="ANNOTATOR is categorical", action='store_true')
+    parser.add_argument("-x", "--maximum", help="Maximum value for quantitative annotation", action='store', type=float, default=float('inf'))
+    parser.add_argument("-m", "--minimum", help="Minimum value for quantitative annotation", action='store', type=float, default=float('inf'))
+    parser.add_argument("-i", "--include", help="space separated list of categorical values to filter a categorical ANNOTATOR", nargs="+")
     parser.add_argument("-o", "--output", help="Output path prefix", type=str, default="filter")
     parser.add_argument("-d", "--idOnly", help="Only output variant IDS", action='store_true')
     parser.add_argument("-v", "--verbose", help="Print verbose messages", action='store_true')
     args = parser.parse_args()
-    
+
+    from pandas import DataFrame
+    from pandas import read_csv
+
     try:
         assert (args.isQuantitative != args.isCategorical) or args.getAnnotators
     except AssertionError:
@@ -95,19 +98,20 @@ if __name__ == "__main__":
         print(args.maximum)
         print(args.include is None)
 
-    filter_obj = AnnotationData(infile=args.file, name=args.annotator, mini=args.minimum, maxi=args.maximum, include=args.include, outpath=args.output, only_ids=args.idOnly, q=args.isQuantitative, c=args.isCategorical)
+    filter_obj = AnnotationData(infile=args.FILE, name=args.annotator, mini=args.minimum, maxi=args.maximum, include=args.include, outpath=args.output, only_ids=args.idOnly, q=args.isQuantitative, c=args.isCategorical)
     if args.summarize:
         filter_obj.summarize_annotator()
     elif args.getAnnotators:
         import pandas as pd
-        columns = pd.read_csv(args.file, sep="\t", compression="gzip", nrows=1, header=None)
+        columns = pd.read_csv(args.FILE, sep="\t", compression="gzip", nrows=1, header=None)
         for field in columns.values:
             print(field)
-    else:
+    elif args.filter:
         filter_obj.output_list_of_variants()
         if args.verbose:
             print("Variants saved to:", filter_obj.outfile)
+    else:
+        print("No method specified. Choose one of -g/-s/-f")
 
-    
 
 
