c358cc1c0e63baf8d3c94c5ff4915e6873002f5a
sarah-n-wright <sarahwright474@gmail.com>
2021-08-12 17:40:24 -0700

    Allow override of sex F's, remove out_suff

--- V2/sex_check.sh
+++ V2/sex_check.sh
@@ -1,54 +1,67 @@
 source /nrnb/ukb-majithia/sarah/Git/gwas_pipeline/V2/Configs/$1 "X"
 
-out_suff=$2
+override=$2
+minF=$3
+maxF=$4
 no_split=$(echo ${sex_chr[@]} | grep -c "XY") 
 file_name=${outDir}${outName}.updated_phe
 
-# Don't need this if XY region already separated?
-if [ $no_split -eq 0 ]
+echo $override
+echo $minF
+echo $maxF
+
+if [ "$override" -eq 1 ]
 then
+	echo "OVERRIDING CONFIG THRESHOLDS"
+	sexFmin=$minF
+	sexFmax=$maxF
+	mkdir ${outDir}temp
+	mv ${outDir}${baseName}chrX.sex_check* ${outDir}temp/
+else
+
+  # Don't need this if XY region already separated?
+  if [ $no_split -eq 0 ]
+  then
 	srun -l plink2 --bfile $file_name \
 		--keep ${outDir}${baseName}.pheno.keepID \
 		--split-par $build \
-		--make-bed --out ${outDir}${outName}.sex_split${out_suff}
-	file_name=${outDir}${outName}.sex_split${out_suff}
-fi
+		--make-bed --out ${outDir}${outName}.sex_split
+	file_name=${outDir}${outName}.sex_split
+  fi
 
 # LD prune
-if [ 1 -eq 1 ]
-then
-srun -l plink2 --bfile $file_name \
+  if [ 1 -eq 1 ]
+  then
+  srun -l plink2 --bfile $file_name \
 	--indep-pairwise $LD_window $LD_shift $LD_r2 \
-	--out ${outDir}${outName}.sex_prune${out_suff}
+	--out ${outDir}${outName}.sex_prune
 
 
-srun -l plink2 --bfile $file_name \
-	--extract ${outDir}${outName}.sex_prune${out_suff}.prune.in \
-	--make-bed --out ${outDir}${outName}.temp${out_suff}
-echo "--------------------X chromosome pruned-------------------------"
-
-
-srun -l plink --bfile ${outDir}${outName}.temp${out_suff} \
+  srun -l plink2 --bfile $file_name \
+	--extract ${outDir}${outName}.sex_prune.prune.in \
+	--make-bed --out ${outDir}${outName}.temp
+  echo "--------------------X chromosome pruned-------------------------"
+  fi
+fi
+srun -l plink --bfile ${outDir}${outName}.temp \
 	--check-sex $sexFmin $sexFmax \
-	--out ${outDir}${outName}.sex_check${out_suff}
-
+	--allow-extra-chr \
+	--out ${outDir}${outName}.sex_check
 echo "--------------------Sex check performed-------------------------"
 
 
-srun -l python /nrnb/ukb-majithia/sarah/Git/gwas_pipeline/V2/gender_check.py ${outDir}${outName}.sex_check ${outName}${out_suff}
+srun -l python /nrnb/ukb-majithia/sarah/Git/gwas_pipeline/V2/gender_check.py ${outDir}${outName}.sex_check ${outName}
 
 echo "--------------------Figure plotted-------------------------"
 
 
-grep "PROBLEM" ${outDir}${outName}.sex_check${out_suff}.sexcheck | \
-awk '{print $1 "\t" $2}' > ${outDir}${outName}.sex_check${out_suff}.removeID
+grep "PROBLEM" ${outDir}${outName}.sex_check.sexcheck | \
+awk '{print $1 "\t" $2}' > ${outDir}${outName}.sex_check.removeID
 
 echo "--------------------Discord file created-------------------------"
 
-grep -vxF -f ${outDir}${outName}.sex_check${out_suff}.removeID \
+grep -vwF -f ${outDir}${outName}.sex_check.removeID \
 	${outDir}${baseName}.pheno.keepID | \
-	grep -vxF -f ${outDir}${outName}.updated_phe.nosex > \
+	grep -vwF -f ${outDir}${outName}.updated_phe.nosex > \
 	${outDir}${baseName}.sex.keepID
 
-
-fi
